8082a438f9c86f89762ef983fdfcb1cb7db08c45
unknown <lisandro.batiancila@gmail.com>
2024-02-19 23:47:42 +0800

    [S2][misc][sandro] adding env configuration

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,11 +1,8 @@
 import axios from 'axios';
-// require('dotenv').config();
-
-// const BASE_URL = process.env.BASE_URL;
-// const PORT = process.env.PORT;
+import {PORT, BASE_URL} from '@env';
 
 const httpInstance = axios.create({
-  baseURL: "http://192.168.176.134:3000",
+  baseURL: `${BASE_URL}:${PORT}`,
 });
 
 export default httpInstance;
