3b18258ac3b2cac9394b78c7fd2eb4380ce64750
Lisandro Batiancila <BATIANCILA@MLHUILLIER.LOCAL>
2024-03-02 15:50:27 +0800

    [S2][feature][sandro] adding UI for Medical Information

--- src/components/DivContainer/style.tsx
+++ src/components/DivContainer/style.tsx
@@ -3,16 +3,20 @@ import styled from 'styled-components';
 
 type DivContainerProps = {
   alignItems?: string;
+  backgroundColor?: string;
   justifyContent?: string;
   display?: string;
   padding?: string;
   flexDirection?: string;
+  width?: string;
 };
 
 export const DivContainer = styled(View)<DivContainerProps>`
   display: ${(props: DivContainerProps) => props.display};
+  background: ${(props: DivContainerProps) => props.backgroundColor ?? '#FFF'};
   flex-direction: ${(props: DivContainerProps) => props.flexDirection};
   align-items: ${(props: DivContainerProps) => props.alignItems};
   justify-content: ${(props: DivContainerProps) => props.justifyContent};
   padding: ${(props: DivContainerProps) => props.padding ?? 0}px;
+  width: ${(props: DivContainerProps) => props.width ?? '100'}%;
 `;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -8,6 +8,7 @@ import TextInputComponent from '../../components/TextInput';
 import TextInputEnum from '../../enums/TextInput.enum';
 import { useAccountContext } from '../../providers/AccountProvider';
 import { ButtonComponent } from '../../components/Buttons';
+import DivComponent from '../../components/DivContainer';
 
 export default function ProfileDashBoard() {
   const {activeUserInformation} = useAccountContext();
@@ -38,10 +39,38 @@ export default function ProfileDashBoard() {
         </S.ProfileBadgeContainer>
         <DividerComponent margin="10px 0 0 0" />
         <S.ProfileBadgeContainer borderColor={COLOR_LISTS.RED_400} width="90">
-          <TextLabel title="Allergies" fontSize={15} />
-          <TextLabel title="Conditions" fontSize={15} />
-          <TextLabel title="Blood Type" fontSize={15} />
-          <TextLabel title="Do you have Medical Aid?" fontSize={15} />
+          <DivComponent flexDirection="row" justifyContent='space-between'>
+            <TextLabel title="Allergies" fontSize={15} />
+            <DivComponent display="flex" flexDirection="row" justifyContent='flex-end' width='60'>
+              <ButtonComponent title="View" fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+              <TextLabel title="/" textColor={COLOR_LISTS.RED_400} />
+              <ButtonComponent title="Add" fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+            </DivComponent>
+          </DivComponent>
+          <DivComponent flexDirection="row" justifyContent='space-between'>
+            <TextLabel title="Conditions" fontSize={15} />
+            <DivComponent display="flex" flexDirection="row" justifyContent="flex-end" width='60'>
+              <ButtonComponent title="View" fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+              <TextLabel title="/" textColor={COLOR_LISTS.RED_400} />
+              <ButtonComponent title="Add" fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+            </DivComponent>
+          </DivComponent>
+          <DivComponent flexDirection="row" justifyContent='space-between' width='100'>
+            <TextLabel title="Blood Type" fontSize={15} />
+            <DivComponent display="flex" flexDirection="row" justifyContent="flex-end" width='60'>
+              <ButtonComponent title="View" fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+              <TextLabel title="/" textColor={COLOR_LISTS.RED_400} />
+              <ButtonComponent title="Add"fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+            </DivComponent>
+          </DivComponent>
+          <DivComponent flexDirection="row" justifyContent='space-between' width='60'>
+            <TextLabel title="Do you have Medical Aid?" fontSize={15} />
+            <DivComponent display="flex" flexDirection="row" justifyContent="flex-end" width='60'>
+              <ButtonComponent title="View" fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+              <TextLabel title="/" textColor={COLOR_LISTS.RED_400} />
+              <ButtonComponent title="Add" fontSize={15} width={23} textColor={COLOR_LISTS.RED_400} />
+            </DivComponent>
+          </DivComponent>
         </S.ProfileBadgeContainer>
         <DividerComponent margin="10px 0 0 0" />
         <S.ProfileBadgeContainer backgroundColor={COLOR_LISTS.RED_400} width="50">
